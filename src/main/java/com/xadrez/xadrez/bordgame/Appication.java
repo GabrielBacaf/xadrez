853f8530987f188ec28fdd4c7230796b18781659
GabrielBacaf <gabryelbaca1@gmail.com>
2024-12-13 18:08:02 -0400

    featch: creat Position class

---
+++ src/main/java/com/xadrez/xadrez/bordgame/Appication.java
@@ -0,0 +1,9 @@
+package com.xadrez.xadrez.bordgame;
+
+public class Appication {
+    public static void main(String[] args) {
+        Position pos = new Position(5, 3);
+
+        System.out.println("Posição: " + pos);
+    }
+}
